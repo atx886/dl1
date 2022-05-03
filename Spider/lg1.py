8ac98efcb5f74d2301895c15bfa3fef73eb202d1
atx886 <1932245379@qq.com>
2022-05-03 19:53:49 +0800

    Initial commit

--- Spider/lg1.py
+++ Spider/lg1.py
@@ -167,7 +167,7 @@ def zz():
     # 输入号码
     d.find_element_by_xpath(
         '/html/body/uni-app/uni-page/uni-page-wrapper/uni-page-body/uni-view/uni-view[2]/uni-view[2]/uni-view/uni-view/uni-view[2]/uni-view[2]/uni-input/div/input').send_keys(
-        17000653119)
+        18094011187)
     rw()
     # 输入积分
     d.find_element_by_xpath(
